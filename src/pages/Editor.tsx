cc4c51e080a1b01726c757bdc6c70c7c53307ea0
Aashrith Raksh <aashrith.raksh@gmail.com>
2024-09-19 12:00:02 +0530

    implemented the text editor app in the dashboard

--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -1,10 +1,14 @@
+import { HtmlEditor, Image, Inject, Link, QuickToolbar, RichTextEditorComponent, Toolbar } from '@syncfusion/ej2-react-richtexteditor';
 
-const Editor = () => {
-  return (
-    <div>
-      Ediotr page
-    </div>
-  )
-};
+import { Header } from '../components';
 
-export default Editor
+
+const Editor = () => (
+  <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+    <Header category="App" title="Editor" />
+    <RichTextEditorComponent>
+      <Inject services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar]} />
+    </RichTextEditorComponent>
+  </div>
+);
+export default Editor;
