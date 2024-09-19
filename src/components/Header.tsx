03cc08ea69a6b3a7421863866a5dd961089afb4a
Aashrith Raksh <aashrith.raksh@gmail.com>
2024-09-19 11:28:43 +0530

    implemented the calender app in the dashboard

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,15 @@
+import { FC } from "react";
 
-const Header = () => {
-  return (
-    <div>
-      Header
-    </div>
-  )
-};
+const Header: FC<{ category: string; title: string }> = ({
+  category,
+  title,
+}) => (
+  <div className=" mb-10">
+    <p className="text-lg text-gray-400">{category}</p>
+    <p className="text-3xl font-extrabold tracking-tight text-slate-900">
+      {title}
+    </p>
+  </div>
+);
 
-export default Header
+export default Header;
