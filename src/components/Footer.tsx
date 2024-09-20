741507ab45c009ded97a3aa2a4b0d1241d6c861d
Aashrith Raksh <aashrith.raksh@gmail.com>
2024-09-20 14:52:28 +0530

    created Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,10 @@
 
-const Footer = () => {
-  return (
-    <div>
-      Footer
-    </div>
-  )
-};
+const Footer = () => (
+  <div className="mt-24">
+    <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+      © 2022 All rights reserved by Shoppy.com
+    </p>
+  </div>
+);
 
-export default Footer
+export default Footer;
